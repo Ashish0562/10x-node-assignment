f199b1ef504a08df06a2747bd4a2c1c7696cccdf
Ashish kumar <ashish056244@gmail.com>
2022-11-17 21:29:34 +0530

    as1

---
+++ Assignment 1/NODE-FS-CRUD-BOILERPLATE-main/src/first.js
@@ -0,0 +1,6 @@
+// const { myFileWriter, myFileReader, myFileDeleter, myFileUpdater } = require(".");
+
+// // myFileWriter('input.txt','hello world');
+// // myFileReader('input.txt').then(data => { console.log(data) });
+// myFileDeleter('inputt.txt')
+// // myFileUpdater('input.txt' , "i am")
