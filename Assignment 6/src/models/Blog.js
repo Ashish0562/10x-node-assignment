0dc15e246ae4485ea73c4f93a84e7a8544358e70
Ashish kumar <ashish056244@gmail.com>
2022-11-18 10:01:05 +0530

    assignment6

---
+++ Assignment 6/src/models/Blog.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+const blogSchema = new mongoose.Schema({
+  // Your code goes here
+  topic: { type: String, required: true },
+  description: { type: String, required: true },
+  posted_at: { type: String, required: true },
+  posted_by: { type: String, required: true }
+});
+
+const Blog = mongoose.model("blogs", blogSchema);
+
+module.exports = Blog;
